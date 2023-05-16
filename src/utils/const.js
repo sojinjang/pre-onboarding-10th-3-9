7b107c36e21a9ce0aa5434481b0af5717fcfa483
sojinjang <jsojin6119@gmail.com>
2023-05-16 10:51:52 +0900

    refactor: 상수 파일 분리

---
+++ src/utils/const.js
@@ -0,0 +1,8 @@
+export const DEBOUNCE_DELAY_IN_MS = 500;
+export const START_ACTIVE_INDEX = -1;
+
+export const KEYBOARD = Object.freeze({
+  ARROW_UP: 'ArrowUp',
+  ARROW_DOWN: 'ArrowDown',
+  ENTER: 'Enter',
+});
